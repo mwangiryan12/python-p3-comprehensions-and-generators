261c51df13dd7da1d421f6c8bff8d85118d2271c
mwangiryan12 <mwangiryan12@gmail.com>
2024-06-04 20:06:26 +0300

    initial commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [n for n in num_list if n%2==0]
 
 def make_exclamation(sentence_list):
+    return [n+"!" for n in sentence_list]
     pass
+print(make_exclamation(["hello","world"]))
